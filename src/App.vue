47e13c10e2b44168701db4b9e65a390978017967
liva <ebammo20@gmail.com>
2023-05-28 08:19:34 +0330

    remove component v model and handle returned value

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,30 @@
 <script setup>
 import {ref} from 'vue';
 import AutoComplete from "./components/jlib/AutoComplete.vue";
-let selectedIem = ref();
-let searchText = ref('a')
+let objs = ref([{key: 1, title:'spain'}])
 let countries =ref([
       {key: 1, title: 'spain' },
       {key: 2, title: 'england' },
       {key: 3, title: 'france' },
       {key: 4, title: 'germany' },
     ]);
-const setItem = (e) => {
-  console.log('item fired.', e);
-  selectedIem.value = e;
+const setItem = (obj, e) => {
+  Object.assign(obj, countries.value.filter(item => item.key === e.key)[0])
 }
 </script>
 
 <template>
-  <div class="w-64">
-    <AutoComplete :search-text="searchText" @update:search-text="nv => searchText = nv" :item-list="countries" @item="setItem"></AutoComplete>
-    <div><span>selected item in parent:</span> <span class="text-blue-900 font-bold">{{selectedIem?.title}}</span></div>
-    <div>{{selectedIem}}</div>
+  <div>
+    <div>
+      <p>objs: </p>
+      <pre><code>{{objs}}</code></pre>
+    </div>
+    <AutoComplete
+        v-for="obj in objs"
+        :selected-object="obj"
+        :item-list="countries"
+        @item="setItem(obj, $event)">
+    </AutoComplete>
   </div>
 </template>
 
--- src/components/jlib/AutoComplete.vue
+++ src/components/jlib/AutoComplete.vue
@@ -1,49 +1,66 @@
 <script setup>
-import {ref} from "vue";
-const props = defineProps(['itemList', 'searchText']);
-const emit = defineEmits(['update:modelValue'])
-
+import {onMounted, ref} from "vue";
+const props = defineProps(['itemList', 'selectedObject']);
+const emit = defineEmits(['update:selectedObject', 'item'])
 let showItems = ref(false)
 let filteredData = ref();
 let selectedItem = ref();
-
+let searchTxt = ref();
+onMounted(() => {
+  selectedItem.value = props.selectedObject;
+  searchTxt.value = selectedItem.value.title;
+})
 const stringIsEmpty = (str) => {
   return str === "";
 }
-
 const search = () => {
-  filteredData.value = props.itemList.filter(item => {
-    console.log(item);
-    return item.title.includes(props.searchText)
-  });
-  showItems.value = !stringIsEmpty(props.searchText.value);
+  filteredData.value = props.itemList.filter(item => item.title.includes(searchTxt.value));
+  showItems.value = !stringIsEmpty(props.selectedObject.title);
 }
 
 const handleSelection = (item) => {
   selectedItem.value = item;
+  searchTxt.value = selectedItem.value.title;
   showItems.value = false;
-  emit('item', selectedItem.value)
+  emit('item', selectedItem.value);
 }
 </script>
 
 <template>
   <div class="relative">
-    <p>st: {{searchText}}</p>
-    <p>it: {{itemList}}</p>
-    <p>fd: {{filteredData}}</p>
-    <input
-        class="border border-gray-200 focus:border-gray-400 shadow-md rounded p-1 w-full"
-        type="text"
-        :value="searchText"
-        @input="$emit('update:searchText', $event.target.value)"
-        @keyup="search">
-    <div v-show="showItems" class="z-20 bg-white border-blue-950 shadow-md absolute w-full rounded" @focusout="showItems=false">
-      <div
-          v-for="d in filteredData"
-          :key="d.key"
-          @click="handleSelection(d)"
-          class="cursor-pointer hover:bg-gray-100 rounded pb-0.5 my-1 mx-1 pl-2"
-      >{{ d.title }}</div>
+    <div class="grid grid-cols-5">
+      <div>
+        <p class="capitalize">selected item</p>
+        <pre><code>{{ selectedItem }}</code></pre>
+      </div>
+      <div>
+        <p class="capitalize">selectedObject</p>
+        <pre><code>{{ selectedObject }}</code></pre>
+      </div>
+      <div class="">
+        <p class="capitalize">item list:</p>
+        <pre><code>{{ itemList }}</code></pre>
+      </div>
+      <div class="">
+        <p class="capitalize">filtered data:</p>
+        <pre><code>{{ filteredData }}</code></pre>
+      </div>
+      <div>
+        <input
+            class="border border-gray-200 focus:border-gray-400 shadow-md rounded p-1 w-full"
+            type="text"
+            :value="searchTxt"
+            @input="searchTxt = $event.target.value"
+            @keyup="search">
+        <div v-show="showItems" class="z-20 bg-white border-blue-950 shadow-md absolute w-full rounded" @focusout="showItems=false">
+          <div
+              v-for="d in filteredData"
+              :key="d.key"
+              @click="handleSelection(d)"
+              class="cursor-pointer hover:bg-gray-100 rounded pb-0.5 my-1 mx-1 pl-2"
+          >{{ d.title }}</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
